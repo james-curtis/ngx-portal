0f9273196e55c33b3006399fab13a16497717ccf
JamesCurtis <jamescurtis@foxmail.com>
2023-11-19 11:28:27 +0800

    chore: npmlog

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import setupRoute from './route';
+import log from './util/logger';
 
 const app = express();
 const port = 3000;
@@ -9,5 +10,5 @@ app.use(express.json());
 setupRoute(app);
 
 app.listen(port, () => {
-  console.log(`ngx-portal listening on port ${port}`);
+  log.info('app', `ngx-portal listening on port ${port}`);
 });
