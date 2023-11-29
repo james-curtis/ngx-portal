a1f501a6e88eaebe0d13bf0a787f42f0c195d688
JamesCurtis <jamescurtis@foxmail.com>
2023-11-29 10:23:59 +0800

    fix: 对接 skydata without group by 数据

--- src/route/index.ts
+++ src/route/index.ts
@@ -2,13 +2,9 @@ import { Express } from 'express';
 import { Portal } from '../controller/portal';
 import { index } from '../controller';
 import { Parse } from '../controller/parse';
-import { SingleSeries } from '../controller/single-series';
-import { MultiSeries } from '../controller/multi-series';
 
 export default function setupRoute(app: Express) {
   app.all('/portal', Portal);
   app.all('/', index);
   app.all('/parse', Parse);
-  app.post('/single-series', SingleSeries);
-  app.post('/multi-series', MultiSeries);
 }
